e12275ddddfae112d0a2b5a6b4fe7954f5cb874a
yannoliv <yannoliv@student.arteveldehs.be>
2019-11-16 14:39:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Worldstarhiphop
---
+++ app/src/main/java/com/example/worldstarhiphop/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.worldstarhiphop.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
